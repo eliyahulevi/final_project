36c356d202d08c69e2808eb3ee98a993980e939d
Shahar Aizikovitz <shahar.aizik@gmail.com>
2021-01-14 00:19:55 +0200

    setters to order class

--- src/main/java/model/order/Order.java
+++ src/main/java/model/order/Order.java
@@ -9,13 +9,43 @@ import model.product.*;
 public class Order 
 {
 	int number;
+	String date;
+	String shipAddress;
+	boolean supplied;
 	User customer;
 	List<Product> products;
 	
-	
+	/*
+	 *  constructor *
+	 */
 	public Order()
 	{
 		
+	}
+	
+	public void setNumber(int n)
+	{
+		this.number = n;
+	}
+	public void setDate(String d)
+	{
+		this.date = d;
+	}
+	public void setAddress(String a)
+	{
+		this.shipAddress = a;
+	}
+	public void setIsSupplied(boolean b)
+	{
+		this.supplied = b;
+	}
+	public void setCustomer(User c)
+	{
+		this.customer = c;
+	}
+	public void addProduct(Product p)
+	{
+		this.products.add(p);
 	}
 
 }
