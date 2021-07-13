37e52de64ef137c2790a95b05170f4a394249bdc
ShaharAizikovitz <shahar.haifa@gmail.com>
2021-07-13 16:48:17 +0300

    change into fade-in scrollable modal

--- src/main/webapp/javascript/order.js
+++ src/main/webapp/javascript/order.js
@@ -9,13 +9,73 @@
 */
 
 
+
+/*********************************************************************************
+*	this function send a new order to the server async. it iterates over all 
+*	'new-products' in the new order and extract details of all added products:
+*	type (catalog), length and color, bundle to an array objects of the following
+*	form: [{'catalog':<catalog>, 'length': <length>, 'color': <color>},..]
+*	@param:		null
+*	return:		null
+*********************************************************************************/
+function sendNewOrder(){
+	var form		= document.getElementById('new-order-form');
+	var products	= form.getElementsByClassName('order-product')	
+	var length		= products.length;
+	
+	if( length === 0 ){
+		alert('no products added..');
+		return;
+	}
+	
+	for(var i = 0; i < length; i++){
+		var lengthElems	= form.getElementsByClassName('label');
+		var length		= new String(lengthElems[0].innerHTML);
+		var color		= new String(lengthElems[1].innerHTML);
+		
+		
+	}
+}
+
+function addProductToOrder1(prodObj){
+	
+	var prodStr		= new String(prodObj).split('#')[1];
+	var prod		= document.getElementById(prodStr);
+	var product		= document.createElement('div');
+	var row			= document.createElement('div');
+	var col9		= document.createElement('div');
+	var col3		= document.createElement('div');
+	var imgs		= prod.getElementsByTagName('img');
+	var img 		= document.createElement('img'); 
+	
+	
+	
+	img.src		= imgs[0].src;
+	
+	col9.setAttribute('class', 'col-xl-9 col-md-8');
+	col3.setAttribute('class', 'col-xl-3 col-md-4 pt-3 pt-md-0');
+	product.setAttribute('class', 'row');
+	product.setAttribute('class', 'container pb-5 mt-n2 mt-md-n3');
+	
+	col3.appendChild(img);
+	row.appendChild(col3);
+	row.appendChild(col9);
+	product.appendChild(row);
+	
+	console.log('add new product 1');
+	return product;
+}
+	
+	
+	
+	
 /*********************************************************************************
 *	this function adds a chosen product to order. according to unique identifier
 *	the function locates the correct 'new-order-product' element and extract
 *	product length and color. after product details are extracted the function
 *	adds the product to the 'new-order-form' form for display
 *	@param:		catalog, int a unique identifier for each product
-*	return:		onull
+*	return:		null
 *********************************************************************************/
 function addProductToOrder(prodObj){
 
@@ -37,20 +97,54 @@ function addProductToOrder(prodObj){
 	var color		= inputs[1].value;
 	var colLen		= new String(color + length);
 	var catLen		= new String(catalog + "_" + length);
+	var row			= document.createElement('div');
+	var colColor	= document.createElement('div');
+	var colLength	= document.createElement('div');
+	var colImage	= document.createElement('div');
+	var colEdRem	= document.createElement('div');
+	//var table		= createTable('productTable', 1, 4, false);
+	var imgURL		= null;
 	
-	console.log('catalog number: ' + catLen);
 	
+	row.setAttribute('class', 'row');	
+	colColor.setAttribute('class', 'col-sm');
+	colLength.setAttribute('class', 'col-sm');
+	colImage.setAttribute('class', 'col-sm');
+	colEdRem.setAttribute('class', 'col-sm');
+	
+	
+	lengthLbl.setAttribute('style', 'text-align: left;');
 	lengthLbl.innerHTML = 'length: ' + length;
 	colorLbl.innerHTML 	= 'color: ' + color; 
-	imgLbl.setAttribute('class', 'image');
+	colorLbl.setAttribute('style', 'margin-left: 15%;');
+	imgLbl.setAttribute('style', 'margin-left: 15%; margin-right: 10%;');
 	imgLbl.src			= img[0].src;
 	edit.innerHTML		= 'edit';
+	imgLbl.setAttribute('class', 'image');
 	edit.setAttribute('onclick', 'editOrderProduct(' + catLen + ')');
+	edit.setAttribute('style', 'margin-left: 35%;');
 	edit.setAttribute('href', '#' + prodStr);
 	remove.setAttribute('onclick', "removeOrderProduct(" + catLen + ")" );
 	remove.setAttribute('href', '#' + prodStr);
+	remove.setAttribute('style', 'text-align:right;');
 	remove.innerHTML	= 'remove';
 	space.innerHTML		= ' / ';
+	/*
+	table.rows[0].cells[0].innerHTML = lengthLbl.innerHTML;
+	table.rows[0].cells[1].innerHTML = colorLbl.innerHTML;
+	table.rows[0].cells[2].innerHTML = "img";
+	table.rows[0].cells[2].appendChild(imgLbl);
+	table.rows[0].cells[3].innerHTML = edit;
+	*/
+	colLength.innerHTNL =(lengthLbl);
+	colColor.appendChild(colorLbl);
+	colImage.appendChild(imgLbl);
+	colEdRem.appendChild(edit)
+	
+	row.appendChild(colColor);
+	row.appendChild(colLength);
+	row.appendChild(colImage);
+	row.appendChild(colEdRem);
 	
 	product.setAttribute('class', 'order-product');
 	product.setAttribute('id', 'order-product-' + catalog + length);
@@ -60,31 +154,100 @@ function addProductToOrder(prodObj){
 	product.appendChild(edit);
 	product.appendChild(space);
 	product.appendChild(remove);
+	
 	form.appendChild(product);
+	form.appendChild(row);
+	
+}
+
+
+function addProductToOrder2(prodObj){
+
+	try{
+		var prodStr		= new String(prodObj).split('#')[1];
+		var product		= document.getElementById(prodStr);
+		var	values		= product.getElementsByTagName('label');
+		var type		= product.catalog;
+		var price		= product.price;
+		var img			= product.getElementsByTagName('img')[0];
+		var form		= document.getElementById('new-order-form');
+		
+		//console.log('product image: ' + img.src);
+		
+		var shopItem = document.createElement("div");
+	  	//shopItem.classList.add("shop-item");
+	  	var shopItemContent = "	<span class='shop-item-title''>" + type + "${type} type pine wood </span> 					\
+		                        <img class='shop-item-image' src=" + img.src + " alt='product photo'> 						\
+		                        <div class='shop-item-details'>																\
+		                        <input class='input-length' type='number' min='0' pattern='[0-9]*' inputmode='numeric'		\
+		                         placeholder='length in centimeters' style='margin-bottom: 3px;'>							\
+		                        <div>																						\
+		                         <input class='input-quantity' type='number' min='0' pattern='[0-9]*' inputmode='numeric'	\
+		                         placeholder='quantity'>																	\
+		                        </div>																						\
+		                        </div>																						\
+		                        <div>																						\
+		                        <span>" + price + "$ per meter</span>														\
+		                        <button role='button' name='add' id='AddToCart' style='margin-top: 5px;'					\
+		                        class='btn btn-primary shop-item-btn'>														\
+		                        <span id='AddToCart'>																		\
+		                         Add To Cart																				\
+		                        </span>" + 																					
+		                        "</button>" +																					
+		                        "</div>";																						
+		/*
+		shopItem
+		    .getElementsByClassName("input-length")[0]
+		    .addEventListener("change", lengthChange);
+		shopItem
+		    .getElementsByClassName("input-quantity")[0]
+		    .addEventListener("change", quantityChange);
+		shopItem
+		  	.getElementsByClassName("shop-item-btn")[0]
+		  	.addEventListener("click", addToCartClicked);
+		*/
+		shopItem.innerHTML = shopItemContent;
+	}
+	catch(error){
+		
+		console.log('error adding item: ' + error);
+	}
+
+	  //var shopItems = document.getElementsByClassName("shop-items")[0];
+	  //shopItems.append(shopItem);
+	 
+	console.log('product added length: ' + values[0].innerHTML + ' and color:' + values[1].innerHTML);
+
+	form.append(shopItem);
 }
 
 
 
 function removeOrderProduct(productID){
 	var str			= new String(productID);
-	console.log(' productID with this: ' + productID);
-	//var product	= new String(productID).split('#')[1];
-	//var prod		= document.getElementById('order-product-' + );
-	//prod.remove();
+	console.log('productID with this: ' + productID);
 	var product		= document.getElementById('order-product-' + productID);
 	var values		= product.getElementsByTagName('label');
 	product.remove();
-	//console.log('remove product with id: ' + str);
+
 	console.log('product id: ' + str + ' has values: ' + values[0].innerHTML + values[1].innerHTML);
 }
 
 
 
 function editOrderProduct(productElement){
-	var str			= new String(productElement);
-	var prodStr		= new String(productElement).split('#')[1];
-	var prod		= document.getElementById(prodStr);
-	console.log('edit product: ' + str);
+	
+	try{
+		var str			= new String(productElement);
+		var prodStr		= new String(productElement).split('#')[1];
+		//var prod		= document.getElementById(prodStr);
+		console.log('edit product: ' + str);
+		
+	}
+	catch(e){
+		console.log('error in edit product: ' + e);
+	}
+
 	
 }
 
@@ -105,7 +268,9 @@ function createNewProductOption(product){
 		var color			= document.createElement('input');
 		var img				= document.createElement('img');
 		var add				= document.createElement('a');
-		var prodObj			= {};
+		var prodObj			= new Object();
+		
+		
 		
 		option.setAttribute('class', 'new-order-product');	
 		option.setAttribute('id', 'new-order-product-' + product.catalog);	
@@ -113,7 +278,8 @@ function createNewProductOption(product){
 		color.setAttribute('id', 'new-product-color-' + product.catalog);
 		add.setAttribute('id', 'new-product-add-' + product.catalog);
 		add.setAttribute('href', '#new-order-product-' + product.catalog);
-		add.setAttribute('onclick', 'addProductToOrder(this)');
+		add.setAttribute('onclick', 'addProductToOrder2(this)');
+		//add.setAttribute('onclick', 'addProductToOrder2(this)');
 		lengthLbl.innerHTML	= 'length: ';
 		colorLbl.innerHTML	= 'color: ';
 		
